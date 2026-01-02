b12cb5d9f1d509e6772ffb71968016041dccdaaf
GodSergio7 <sergiovidalmoreno7@gmail.com>
2026-01-02 20:19:52 +0100

    feat: setup initial structure and items data

---
+++ src/data/items.js
@@ -0,0 +1,22 @@
+export const items = [{
+    id: 1,
+    name: ["Teclado de Membrana", "Teclado Mecanico", "Teclado Custom HHKB"],
+    price: 10, //precio
+    buff: 1, //1 linea por segundo
+    type: "manual"
+},
+{
+    id: 2,
+    name: ["Monitor CRT Viejo", "Monitor 1080p", "Monitor 4K Ultrawide"],
+    price: 100, //precio
+    buff: 2, //2 lineas por segundo
+    type: "auto"
+},
+{
+    id: 3,
+    name: ["Netbook del Gobierno", "Portátil Gaming", "Super Mainframe"],
+    price: 500, //precio
+    buff: 10, //10 lineas por segundo
+    type: "auto"
+}
+];
